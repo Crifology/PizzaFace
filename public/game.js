68146fc9efe449ba3eca196974a195566e3c4efa
Mike Crifo <mike.crifo@gmail.com>
2022-01-03 23:42:36 -0500

    PizzaFace v2.0 Patch

--- public/game.js
+++ public/game.js
@@ -1,77 +1,81 @@
 kaboom({
   global: true,
-  width: 640,
-  height: 480,
-  scale: 2,
+  width: 320,
+  height: 240,
+  scale: 4,
   fullscreen: true,
   debug: true,
   clearColor: [0, 0, 0, 1], // black background, or make third digit a 1 for blue
 });
 
-const MOVE_SPEED = 100;
-const JUMP_FORCE = 500;
-const FALL_DEATH = 700;
+const MOVE_SPEED = 70;
+const JUMP_FORCE = 380;
+const FALL_DEATH = 500;
 
-loadSprite("pizza", "/images/pepp-pizza.png");
-loadSprite("zombie", "/images/FatZombieLeft.png");
-loadSprite("brick", "/images/Brick2.png");
-loadSprite("PizzaGuyRight", "/images/pizzaguyright.png");
-loadSprite("PizzaGuyLeft", "/images/pizzaguyleft.png");
+loadSprite("pizza", "/images/pizza.png");
+loadSprite("zombieleft", "/images/ZLeft.png");
+loadSprite("zombieright", "/images/ZRight.png");
+loadSprite("brick", "/images/Brick.png");
+loadSprite("PizzaGuyRight", "/images/PGRight.png");
+loadSprite("PizzaGuyLeft", "/images/PGLeft.png");
 loadSprite("HauntingGhost", "/images/Haunting-Ghost.png");
 loadSprite("Doorway", "/images/Doorway.png");
-
-
 
 scene("game", ({ level, score }) => {
   layers(["bg", "obj", "ui"], "obj");
 
-  const gameboard = [
+  const LEVELS = [
     [
-      "                                           $$             #",
-      " $          #         $$      $   $$   #$       $          ",
-      "    $           $                            $    $    # $ ",
-      "==       =====   =    ===    ===  =    =  &  =    =   =    ",
-      "    $ $       $     $  $       #     $$        $      $    ",
-      "             &              $                     $    $  $",
-      "    ===               ==     $       ==        =      =  ==",
-      "$$       $$    #     $ $   #$      #    $ #        $      ^",
-      "                      $                            $    $  ",
-      "=====  =====  =====  ===  ====  =====  ====  ==  ===   ====",
-      "                                                           ",
+      "           #   $            $      #                  $    =",
+      "= $    =      =         $   =     $   $=      $     =      =",
+      "===  ====  ====  ====  ==  ===  ===  ===  ==  ===  ====   ==",
+      "=                                                          =",
+      "=                  $                                       =",
+      "=      $   $                =                              =",
+      "= ==   ===  == === ===   ====   =  ==  ==  ==  ====  =  ====",
+      "$                                                          =",
+      "                      =                                    =",
+      "===   =   =     ===           =   ==     =      ==    =    =",
+      "              $           $   $     $                      =",
+      "$   $   =    =          =                       $    $     =",
+      "===    === ===   =  =  ===   ===  =    =  === ==  =   ===  =",
+      "=     $       $     $  $          $           $      $     =",
+      "=   =                                =                    $=",
+      "=  ===       &        =             ===    =      ==      ==",
+      "=    $                      $           $ #                 ",
+      "=        =              $ =                  =   $     $  ^ ",
+      "=====  =====  ===== ====  ====  ====   ====  == ====  ======",
     ],
     [
-      "==$                 $                                      ",
-      "           $$ $     =   $$           & =      #     $   $  ",
-      "               #    =         $$       =    $$      $   #  ",
-      "           ==  =   ==   =    ===      ===   ===    ===   $$",
-      "=&              #       $         #              #      #==",
-      "    $          $$          $            $$       $$        ",
-      " $  =  ==      ===      =     $  $   & ====      == & $    ",
-      "       #                  #      =          #             #",
-      "$$       $$          $$$      $  =                    $$  ^",
-      "==     ====   ====  =======  ==  =   = ===  ==  =  =  =====",
-      "                                                           ",
+      "   $ =======================  ==== = =============  =  =====",
+      "=              $                          $               $=",
+      "=   &        $  $                     $        $$    =     =",
+      "=====  ==   ==  =  =   =$= =$=  =  == =  ===  ==== =====  ==",
+      "               #                              #             ",
+      "  $$      =        =      $    =        $     $     =       ",
+      "= == == =====  == ===   ====  ===  ===  = = = = = ===  =  ==",
+      "=                                                 &        =",
+      "  $            #                 $$                        =",
+      "        =    $   $ =        $          =     $     $    $  =",
+      "=====  ===  ======   =====  = =  === =====  ===  ====   =  =",
+      "= &                                                        =",
+      "=                  $          #         $                  =",
+      "= $     $   $     ===     $  $       =  ==  ==    $      $$=",
+      "= = = = = = = = ========  == = = = ============ = = = = ====",
+      "=            &                                              ",
+      "     $             #       $                                ",
+      " $       =       $         ==    $          $=    $         ",
+      "=====  =====  ===== ====  ====  ==== = ===  ===  ===  ======",
     ],
-    [
-      "                                       =   $$  $$ &=$$$    ",
-      " $$$       $$ $$      $$$      $$    $$$                   ",
-      "        &                                                  ",
-      "====     ===  ==   == =   ===   ==   ====   ===   == = = ==",
-      "    $$ $           $$  $$            & $$  $$$      $$   $$",
-      "  =                            $                 $  =      ",
-      "    ===  =    ==   ==     ==   =     ==          =    =  ==",
-      "    $$$ $    $$$$     $$$   $$$        &$$$     $$ $   $  ^",
-      "=                                                  =       ",
-      "= = ====  ==  == =  === =  === = =  ===  ==   ==  ==== =  =",
-      "                                                           ",
-    ],
+    
   ];
 
-  let gamemusic = new Howl({
+
+  const gamemusic = new Howl({
       src: ['./audio/gamemusic.mp3'],
       autoplay: true,
       loop: true,
-      volume: 0.25
+      volume: 0.25,
     }) 
 
   let falldeath = new Howl({
@@ -83,28 +87,37 @@ scene("game", ({ level, score }) => {
     src: ['./audio/BitingFood.wav'],
     volume: 0.5,
   })
+
+  let ghostdeath = new Howl({
+    src: ['./audio/GhostDeath.wav'],
+    volume: 0.5,
+  })
+
+  let zombiedeath = new Howl({
+    src: ['./audio/Zombie Brains.wav'],
+    volume: 0.5,
+  })
   
   gamemusic.play();
   Howler.volume(0.3);
 
   const levelConfig = {
-    width: 32,
-    height: 32,
+    width: 17,
+    height: 17,
     "=": [sprite("brick"), solid(), "brick"],
     "$": [sprite("pizza"), "pizza"],
-    "#": [sprite("zombie"), solid(), "zombie"],
+    "#": [sprite("zombieleft"), solid(), "zombie", { dir: -1 }],
     "&": [sprite("HauntingGhost"), solid(), "ghost", { dir: -1 }],
     "^": [sprite("Doorway"), "door"],
   };
 
-  const gameLevel = addLevel(gameboard[level], levelConfig);
+  const gameLevel = addLevel(LEVELS[level], levelConfig);
 
   const scoreLabel = add([
     text("Pizza Score: " + score),
-    pos(20, 0),
-    pos(400, 0),
+    pos("top"),
     layer("ui"),
-    scale(2),
+    scale(2.5),
     {
       value: score,
     },
@@ -112,18 +125,18 @@ scene("game", ({ level, score }) => {
 
   add([
     text("Level " + parseInt(level + 1)),
-    pos(width() / 2, height() / 2),
+    pos(width() / 2, height() / 10),
     layer("ui"),
     {
       value: score,
     },
-    scale(2),
+    scale(2.5),
   ]);
 
   const player = add([
     sprite("PizzaGuyRight"),
     solid(),
-    pos(30, 0),
+    pos(5, 0),
     body(),
     origin("bot"),
     layer("obj"),
@@ -138,34 +151,39 @@ scene("game", ({ level, score }) => {
 
   player.collides("zombie", (d) => {
     gamemusic.stop()
+    zombiedeath.play()
     go("lose2", { score: scoreLabel.value });
   });
 
   player.collides("ghost", (d) => {
     gamemusic.stop()
+    ghostdeath.play()
     go("lose2", { score: scoreLabel.value });
   });
 
-  const ENEMY_SPEED = 85;
-  const GHOST_SPEED = 125;
+  const ENEMY_SPEED = 50;
+  const GHOST_SPEED = 70;
 
   action("zombie", (d) => {
-    d.move(-ENEMY_SPEED, 0);
+    d.move(d.dir * -ENEMY_SPEED, 0)
   });
 
   action("ghost", (d) => {
-    d.move(d.dir * GHOST_SPEED, 0);
+    d.move(d.dir * -GHOST_SPEED, 0);
   });
 
   collides("ghost", "brick", (g) => {
     g.dir = -g.dir;
   });
 
+  collides("zombie", "brick", (g) => {
+    g.dir = -g.dir;
+  });
+
   player.overlaps("door", () => {
+
     gamemusic.stop()
-    wait(2)
-    gamemusic.play()
-    Howler.volume(0.3)
+  
     go("game", {
       level: level + 1,
       score: scoreLabel.value,
@@ -199,7 +217,7 @@ scene("game", ({ level, score }) => {
 
 scene("lose", ({ score }) => {
   add([
-    text("YOU FELL! \n\nSCORE: " + score + "\n\nRefresh Browser for \n\nNew Game!", 32),
+    text("YOU FELL! \n\nSCORE: " + score + "\n\nRefresh Browser for \n\nNew Game!", 18),
     origin("center"),
     pos(width() / 2, height() / 2)
   ])
@@ -207,10 +225,11 @@ scene("lose", ({ score }) => {
 
 scene("lose2", ({ score }) => {
   add([
-    text("UNDEAD ATE YOU! \n\nSCORE: " + score + "\n\nRefresh Browser for \n\nNew Game!" , 32),
+    text("UNDEAD ATE YOU! \n\nSCORE: " + score + "\n\nRefresh Browser for \n\nNew Game!" , 18),
     origin("center"),
     pos(width() / 2, height() / 2)
   ])
+
 });
 
-start("game", { level: 0, score: 0 });
+start("game", {level: 0, score: 0});
